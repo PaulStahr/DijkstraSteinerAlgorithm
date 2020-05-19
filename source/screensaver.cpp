8780bc1e9e4e7a87ef9a17ad7d06a5b58aee1dd9
Paul Stahr <paul.stahr@gmx.de>
2020-05-19 17:56:09 +0200

    Added licence-text

--- source/screensaver.cpp
+++ source/screensaver.cpp
@@ -1,3 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Paul Stahr
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+
 #include <SDL/SDL.h>
 #include <SDL/SDL_opengl.h>
 #include <math.h>
@@ -8,11 +30,6 @@
 
 #include "application_window.h"
 
-using std::cout;
-using std::endl;
-
-
-// Polling-Funktion für Events
 bool pollEvents()
 {
   SDL_Event event;
@@ -80,7 +97,7 @@ int main(int , char **)
 {
     if( SDL_Init( SDL_INIT_VIDEO ) != 0 )
     {
-        cout << "Can't load sld " << SDL_GetError() << ')' << endl;
+        std::cout << "Can't load sld " << SDL_GetError() << ')' << std::endl;
         return 1;
     }
 
